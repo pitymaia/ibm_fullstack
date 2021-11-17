fc0724df2c8b39b2c07774685ef4af3786432cdd
andre <pitymaia@gmail.com>
2021-11-16 23:43:21 -0300

    Flask version

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn -w 4 server:mailing
+web: gunicorn -w 4 -b 0.0.0.0:$PORT server:app
--- server.py
+++ server.py
@@ -1,7 +1,7 @@
-import json
 import requests
+import json
 
-from flask import Flask
+from flask import Flask, jsonify, request, make_response
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -9,36 +9,23 @@ CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 
 @app.route("/", methods=['GET', 'POST', 'OPTIONS'])
-def mailing(environ, start_response):
-    if environ['REQUEST_METHOD'] == 'OPTIONS':
+def mailing():
+    if request.method == 'OPTIONS':
         return _build_cors_preflight_response()
-    elif environ['REQUEST_METHOD'] == 'POST':
-        try:
-            request_body_size = int(environ['CONTENT_LENGTH'])
-            request_body = environ['wsgi.input'].read(request_body_size)
-            json_data = json.loads(request_body)
-            getresponse_headers = {
-                'Content-Type': 'application/json',
-                'X-Auth-Token': 'api-key 8dm28sn76d67e0ho3j1y6x7wonfqhcm5',
-            }
+    elif request.method == 'POST':
+        json_data = json.loads(request.data)
+        getresponse_headers = {
+            'Content-Type': 'application/json',
+            'X-Auth-Token': 'api-key 8dm28sn76d67e0ho3j1y6x7wonfqhcm5',
+        }
 
-            getresponse_data = {"campaign": {"campaignId": json_data['id']}, "email": json_data['email']}
+        getresponse_data = {"campaign": {"campaignId": json_data['id']}, "email": json_data['email']}
 
-            response = requests.post('https://api.getresponse.com/v3/contacts', headers=getresponse_headers, data=json.dumps(getresponse_data))
-        except (TypeError, ValueError):
-            request_body = "0"
-
-        status = '200 OK'
-        headers = [('Content-type', 'text/plain'), ('Access-Control-Allow-Origin', '*')]
-        start_response(status, headers)
-        return response
+        resp = requests.post('https://api.getresponse.com/v3/contacts', headers=getresponse_headers, data=json.dumps(getresponse_data))
+        return (resp.text, resp.status_code, resp.headers.items())
     else:
         response_body = open('index.html').read()
-        status = '200 OK'
-        headers = [('Content-type', 'text/html'),
-                   ('Content-Length', str(len(response_body)))]
-        start_response(status, headers)
-        return [response_body.encode("utf-8")]
+        return response_body
 
 def _build_cors_preflight_response():
     response = make_response()
@@ -46,3 +33,8 @@ def _build_cors_preflight_response():
     response.headers.add("Access-Control-Allow-Headers", "*")
     response.headers.add("Access-Control-Allow-Methods", "*")
     return response
+
+
+if __name__ == '__main__':
+    app.run()
+   # app.run(host = '0.0.0.0',port=7000, debug=True)
