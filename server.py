7738ae671b9b5236d06019a1fc7bff06e3d0be1b
andre <pitymaia@gmail.com>
2021-11-17 16:10:46 -0300

    Allow get API from client

--- server.py
+++ server.py
@@ -14,14 +14,18 @@ def mailing():
         return _build_cors_preflight_response()
     elif request.method == 'POST':
         json_data = json.loads(request.data)
+        api_key = json_data['key'] if hasattr(json_data, 'key') else '8dm28sn76d67e0ho3j1y6x7wonfqhcm5'
         getresponse_headers = {
             'Content-Type': 'application/json',
-            'X-Auth-Token': 'api-key 8dm28sn76d67e0ho3j1y6x7wonfqhcm5',
+            'X-Auth-Token': 'api-key {0}'.format(api_key)
         }
 
         getresponse_data = {"campaign": {"campaignId": json_data['id']}, "email": json_data['email']}
 
-        resp = requests.post('https://api.getresponse.com/v3/contacts', headers=getresponse_headers, data=json.dumps(getresponse_data))
+        resp = requests.post(
+            'https://api.getresponse.com/v3/contacts',
+            headers=getresponse_headers, data=json.dumps(getresponse_data)
+        )
         return (resp.text, resp.status_code, resp.headers.items())
     else:
         response_body = open('index.html').read()
